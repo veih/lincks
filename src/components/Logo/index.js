a0e40671d6f41be03c3fe1cb4c300bdf506563a5
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-11-23 21:53:39 -0300

    adicionando pages

---
+++ src/components/Logo/index.js
@@ -0,0 +1,9 @@
+import './logo.css'
+
+export function Logo() {
+    return(
+        <h1>
+            DevVeih
+        </h1>
+    )
+}
--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom'
+import { Logo } from '../../components/Logo'
 
 import './Error.css'
 
 export default function Error() {
     return (
         <div className='error'>
+          <Logo />
           <h1>Página não encontrada:</h1>
 
           <Link className='link' to='/'>
